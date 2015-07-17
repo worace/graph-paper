0019bfa93e2a079ffa600a6fbb4803079d7a4f7d
worace <horace.d.williams@gmail.com>
2015-07-17 15:37:51 -0600

    removing canvas code long may it live in git

--- src/graph_paper/core.cljs
+++ src/graph_paper/core.cljs
@@ -6,26 +6,6 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
-(defn set-font [canvas size font]
-  (set! (.-font canvas) (str size "px " font)))
-
-(defn make-canvas []
-  (.createElement js/document "canvas"))
-
-(defonce canvas (.appendChild (first (h/by-tag "body")) (make-canvas)))
-
-(defn context [canvas] (.getContext canvas "2d"))
-(defn clear [canvas]
-  (.clearRect (context canvas) 0 0 (.-width canvas) (.-height canvas)))
-
-(defn render-text [canvas text]
-  (let [ctx (context canvas)]
-    (set-font ctx 20 "Courier New")
-    (.fillText ctx text 10 30)))
-
-(clear canvas)
-(render-text canvas (:text @app-state))
-
 (defn graph-paper []
   [:h1 (:text @app-state)
    [:div
